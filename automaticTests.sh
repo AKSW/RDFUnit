fe5eba69bd4322c5dc1b3fea335a2f8b23305cb3
kurzum <kurzum@github.com>
2013-05-03 11:00:36 +0200

    new endpoints + functional test results

---
+++ automaticTests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+. "$1"
+OUTPUT=autotests/functional."$NAME".tex
+
+echo "" > $OUTPUT
+#curl http://mappings.dbpedia.org/server/ontology/dbpedia.owl
+for i in `cat dbpedia.owl  | rapper -I - - file | grep FunctionalProperty | cut -f1 -d '>' | sed 's/<//' | sort -u `
+do
+
+		PNAME=$(echo "$i" | sed 's/.*\///g')
+		
+		PREV=`curl $ENDPOINT  --data-urlencode  default-graph-uri=$DEFAULTGRAPH -d format=csv --data-urlencode query="SELECT COUNT(?s) { ?s <$i> ?c. } "  | tail -1 `
+		ERROR=`curl $ENDPOINT  --data-urlencode  default-graph-uri=$DEFAULTGRAPH -d format=csv --data-urlencode query="SELECT COUNT(?grpdEntry) { { SELECT COUNT(?s) AS ?grpdEntry{ ?s <$i> ?c.  }   GROUP BY ?s  HAVING count(?c) > 1} } "  | tail -1 `
+
+		#echo "$RATE & $PNAME & $PREV & $ERROR \\" 
+		RATE=`echo "scale=4\n($ERROR/$PREV) * 100" | bc `
+		echo "$RATE & $PNAME & $PREV & $ERROR \\" >> $OUTPUT
+
+done
+sed -i 's/^ &/0&/;s/00 / /;s/^\./0\./' $OUTPUT
+sort -n -r -o $OUTPUT $OUTPUT
+ 
