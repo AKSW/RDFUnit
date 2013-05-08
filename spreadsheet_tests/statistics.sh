b76bd01b834413145950af9e361f28af9aed221a
Dimitris Kontokostas <jimkont@gmail.com>
2013-05-08 16:11:19 +0300

    statistics generation (2)

--- spreadsheet_tests/statistics.sh
+++ spreadsheet_tests/statistics.sh
@@ -4,11 +4,11 @@ do
 	template=`echo $LINE | cat | cut -d " " -f 1`
 	instance=`echo $LINE | cat | cut -d " " -f 2`
 	
-	outputTsvFile=tmp_$template_$instance.tsv
+	outputTsvFile=tmp_$template$instance.tsv
 	cat results.tsv | grep -P "$template\t$instance" | cut -f 3-5> $outputTsvFile
 	
-	outputPngFile=image_$template_$instance.png
-	gnuplot -e "filename='$outputTsvFile'" results.gpl > $outputPngFile
+	outputPngFile=image_errprev_$template$instance.png
+	gnuplot -e "filename='$outputTsvFile'" results-error-prevalence.gpl > $outputPngFile
 done
 
 rm tmp*.tsv
