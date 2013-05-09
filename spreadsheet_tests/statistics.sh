fe294d614727a269df9cf4a9a6eb5afd261938d8
Dimitris Kontokostas <jimkont@gmail.com>
2013-05-09 15:36:46 +0300

    statistics generation (2)

--- spreadsheet_tests/statistics.sh
+++ spreadsheet_tests/statistics.sh
@@ -1,14 +1,17 @@
 
-cat results.tsv | cut -f 1-2 | sort -u | grep -v "^#" | sed 's/\t/ /g'| while read LINE;
+cat results.tsv | sed 's/{/0/g' | cut -f 1-2 | sort -u | grep -v "^#" | sed 's/\t/ /g'| while read LINE;
 do
 	template=`echo $LINE | cat | cut -d " " -f 1`
 	instance=`echo $LINE | cat | cut -d " " -f 2`
 	
 	outputTsvFile=tmp_$template$instance.tsv
-	cat results.tsv | grep -P "$template\t$instance" | cut -f 3-5> $outputTsvFile
+	cat results.tsv | grep -P "$template\t$instance" | cut -f 3-6> $outputTsvFile
 	
 	outputPngFile=image_errprev_$template$instance.png
 	gnuplot -e "filename='$outputTsvFile'" results-error-prevalence.gpl > $outputPngFile
+	
+	outputPngFile=image_errate_$template$instance.png
+	gnuplot -e "filename='$outputTsvFile'" results-error-rate.gpl > $outputPngFile
 done
 
-rm tmp*.tsv
+#rm tmp*.tsv
