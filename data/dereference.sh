f28a66b890b69b52ec4ebf562ff21422463715d5
Dimitris Kontokostas <jimkont@gmail.com>
2013-12-17 10:38:22 +0200

    provide rdf/xml dereferencing

--- data/dereference.sh
+++ data/dereference.sh
@@ -2,3 +2,7 @@ find -name *.tests.Schema.ttl | xargs cat > tests.auto.ttl
 find -name *.tests.Manual.ttl | xargs cat > tests.manual.ttl
 find -name *.tests.EnrichedSchema.ttl | xargs cat > tests.enriched.ttl
 find -name *.results.ttl | xargs cat > tests.ttl
+
+rapper -i turtle -o rdfxml patterns.ttl           > patterns.rdf
+rapper -i turtle -o rdfxml testAutoGenerators.ttl > testAutoGenerators.rdf
+rapper -i turtle -o rdfxml tests.ttl              > tests.rdf
