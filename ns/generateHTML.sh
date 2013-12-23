3223967b332e23e1e76445ac143bd3fa320ab04f
Dimitris Kontokostas <jimkont@gmail.com>
2013-12-23 11:20:16 +0200

    fix script too

--- ns/generateHTML.sh
+++ ns/generateHTML.sh
@@ -1,1 +1,1 @@
-wget http://www.essepuntato.it/lode/https://raw.github.com/AKSW/Databugger/master/ontology/core.rdf -O core.html
+wget http://www.essepuntato.it/lode/https://raw.github.com/AKSW/Databugger/master/ns/core.rdf -O core.html
