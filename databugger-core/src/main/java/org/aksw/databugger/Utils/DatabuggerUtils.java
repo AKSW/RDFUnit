ecef6706ce4fc1d0dc9ed1b495ea5fe080599d9e
Dimitris Kontokostas <jimkont@gmail.com>
2014-02-23 16:42:47 +0100

    LOV fetching in try/catch

--- databugger-core/src/main/java/org/aksw/databugger/Utils/DatabuggerUtils.java
+++ databugger-core/src/main/java/org/aksw/databugger/Utils/DatabuggerUtils.java
@@ -91,38 +91,49 @@ public class DatabuggerUtils {
         List<Source> sources = new ArrayList<Source>();
         Source lov = new DatasetSource("lov", "http://lov.okfn.org", "http://lov.okfn.org/endpoint/lov", "", null);
 
-        // Example from http://lov.okfn.org/endpoint/lov
-        QueryExecution qe = lov.getExecutionFactory().createQueryExecution(
-                "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                        "PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>\n" +
-                        "PREFIX dcterms:<http://purl.org/dc/terms/>\n" +
-                        "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>\n" +
-                        "PREFIX owl:<http://www.w3.org/2002/07/owl#>\n" +
-                        "PREFIX skos:<http://www.w3.org/2004/02/skos/core#>\n" +
-                        "PREFIX foaf:<http://xmlns.com/foaf/0.1/>\n" +
-                        "PREFIX void:<http://rdfs.org/ns/void#>\n" +
-                        "PREFIX bibo:<http://purl.org/ontology/bibo/>\n" +
-                        "PREFIX vann:<http://purl.org/vocab/vann/>\n" +
-                        "PREFIX voaf:<http://purl.org/vocommons/voaf#>\n" +
-                        "PREFIX frbr:<http://purl.org/vocab/frbr/core#>\n" +
-                        "PREFIX lov:<http://lov.okfn.org/dataset/lov/lov#>\n" +
-                        "\n" +
-                        "SELECT ?vocabPrefix ?vocabURI \n" +
-                        "WHERE{\n" +
-                        "\t?vocabURI a voaf:Vocabulary.\n" +
-                        "\t?vocabURI vann:preferredNamespacePrefix ?vocabPrefix.\n" +
-                        "} \n" +
-                        "ORDER BY ?vocabPrefix ");
-
+        QueryExecution qe = null;
         int count = 0;
-        ResultSet rs = qe.execSelect();
-        while (rs.hasNext()) {
-            QuerySolution row = rs.next();
 
-            String prefix = row.get("vocabPrefix").toString();
-            String vocab = row.get("vocabURI").toString();
-            SchemaService.addSchemaDecl(prefix, vocab);
-            count++;
+
+        try {
+            // Example from http://lov.okfn.org/endpoint/lov
+            qe = lov.getExecutionFactory().createQueryExecution(
+                    "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                            "PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>\n" +
+                            "PREFIX dcterms:<http://purl.org/dc/terms/>\n" +
+                            "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>\n" +
+                            "PREFIX owl:<http://www.w3.org/2002/07/owl#>\n" +
+                            "PREFIX skos:<http://www.w3.org/2004/02/skos/core#>\n" +
+                            "PREFIX foaf:<http://xmlns.com/foaf/0.1/>\n" +
+                            "PREFIX void:<http://rdfs.org/ns/void#>\n" +
+                            "PREFIX bibo:<http://purl.org/ontology/bibo/>\n" +
+                            "PREFIX vann:<http://purl.org/vocab/vann/>\n" +
+                            "PREFIX voaf:<http://purl.org/vocommons/voaf#>\n" +
+                            "PREFIX frbr:<http://purl.org/vocab/frbr/core#>\n" +
+                            "PREFIX lov:<http://lov.okfn.org/dataset/lov/lov#>\n" +
+                            "\n" +
+                            "SELECT ?vocabPrefix ?vocabURI \n" +
+                            "WHERE{\n" +
+                            "\t?vocabURI a voaf:Vocabulary.\n" +
+                            "\t?vocabURI vann:preferredNamespacePrefix ?vocabPrefix.\n" +
+                            "} \n" +
+                            "ORDER BY ?vocabPrefix ");
+
+
+            ResultSet rs = qe.execSelect();
+            while (rs.hasNext()) {
+                QuerySolution row = rs.next();
+
+                String prefix = row.get("vocabPrefix").toString();
+                String vocab = row.get("vocabURI").toString();
+                SchemaService.addSchemaDecl(prefix, vocab);
+                count++;
+            }
+        } catch (Exception e) {
+            //TODO log error about lov
+        } finally {
+            if (qe != null)
+                qe.close();
         }
 
         log.info("Loaded " + count + " schema declarations from LOV SPARQL Endpoint");
