4b21940512b10aa1dff93dd04654998d24b755c1
Dimitris Kontokostas <jimkont@gmail.com>
2014-03-19 10:24:38 +0200

    minor renaming on the ontology

--- ns/dereferenceNS.sh
+++ ns/dereferenceNS.sh
@@ -5,4 +5,4 @@ rapper -i turtle -o rdfxml-abbrev core.ttl > core.rdf
 wget "http://mowl-power.cs.man.ac.uk:8080/converter/convert?ontology=http://rdfunit.aksw.org/ns/core.ttl&format=OWL/XML"  -O core.owl
 
 # HTML Display
-wget http://www.essepuntato.it/lode/https://raw.github.com/AKSW/Databugger/master/ns/core.rdf -O core.html
+wget http://www.essepuntato.it/lode/https://raw.github.com/AKSW/RDFUnit/master/ns/core.rdf -O core.html
