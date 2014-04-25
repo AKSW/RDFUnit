5e975addf2a355c2fa7ab05636655c63ad2fdf61
Dimitris Kontokostas <jimkont@gmail.com>
2014-04-25 11:01:35 +0300

    Implement Status Executor

--- rdfunit-core/src/main/java/org/aksw/rdfunit/tests/executors/StatusTestExecutor.java
+++ rdfunit-core/src/main/java/org/aksw/rdfunit/tests/executors/StatusTestExecutor.java
@@ -1,8 +1,15 @@
 package org.aksw.rdfunit.tests.executors;
 
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.sparql.engine.http.QueryExceptionHTTP;
+import org.aksw.rdfunit.Utils.SparqlUtils;
+import org.aksw.rdfunit.enums.TestCaseResultStatus;
 import org.aksw.rdfunit.sources.Source;
 import org.aksw.rdfunit.tests.TestCase;
+import org.aksw.rdfunit.tests.results.StatusTestCaseResult;
 import org.aksw.rdfunit.tests.results.TestCaseResult;
+
+import java.util.Arrays;
 
 
 /**
@@ -13,6 +20,29 @@ import org.aksw.rdfunit.tests.results.TestCaseResult;
 public class StatusTestExecutor extends TestExecutor {
     @Override
     protected java.util.Collection<TestCaseResult> executeSingleTest(Source source, TestCase testCase) {
-        return null;
+
+        TestCaseResultStatus status = TestCaseResultStatus.Error;
+        QueryExecution qe = null;
+
+        try {
+            qe = source.getExecutionFactory().createQueryExecution(testCase.getSparqlAsAskQuery());
+            boolean fail = qe.execAsk();
+
+            if (fail)
+                status = TestCaseResultStatus.Fail;
+            else
+                status = TestCaseResultStatus.Success;
+
+        } catch (QueryExceptionHTTP e) {
+            if (SparqlUtils.checkStatusForTimeout(e))
+                status = TestCaseResultStatus.Timeout;
+
+        } finally {
+            if (qe != null)
+                qe.close();
+        }
+
+        return Arrays.<TestCaseResult>asList(new StatusTestCaseResult(testCase, status));
     }
+
 }
