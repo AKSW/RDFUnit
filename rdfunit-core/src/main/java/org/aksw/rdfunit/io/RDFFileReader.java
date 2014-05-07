15d6ecf459072cfca46baa4f9a00457bc25d3664
Dimitris Kontokostas <jimkont@gmail.com>
2014-05-07 15:19:53 +0300

    Adapt RDFFileReader to accept InputStreams as well This is useful to load resources

--- rdfunit-core/src/main/java/org/aksw/rdfunit/io/RDFFileReader.java
+++ rdfunit-core/src/main/java/org/aksw/rdfunit/io/RDFFileReader.java
@@ -5,6 +5,8 @@ import org.aksw.rdfunit.exceptions.TripleReaderException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 /**
  * User: Dimitris Kontokostas
@@ -12,27 +14,44 @@ import java.io.FileInputStream;
  * Created: 11/14/13 8:37 AM
  */
 public class RDFFileReader extends DataReader {
-    private final String filename;
+    private final InputStream file;
     private final String format;
 
     public RDFFileReader(String filename) {
-        this.filename = filename;
-        this.format = "TURTLE";
+        this(getStreamFromFilename(filename),"TURTLE");
+    }
+
+    public RDFFileReader(InputStream file) {
+        this(file, "TURTLE");
     }
 
     public RDFFileReader(String filename, String format) {
-        this.filename = filename;
+        this(getStreamFromFilename(filename),format);
+    }
+
+    public RDFFileReader(InputStream file, String format) {
+        this.file = file;
         this.format = format;
     }
 
     @Override
     public void read(Model model) throws TripleReaderException {
         try {
-            File f = new File(filename);
-            model.read(new FileInputStream(f), null, format);
+            model.read(file, null, format);
         } catch (Exception e) {
             throw new TripleReaderException(e.getMessage());
         }
 
+    }
+
+    // Returns a FileInputStream or null in case of exception
+    // We want to raise the exception only at ready time
+    private static InputStream getStreamFromFilename(String filename) {
+        try {
+            return new FileInputStream(filename);
+        } catch (FileNotFoundException e) {
+            // do not handle exception at construction time
+            return null;
+        }
     }
 }
