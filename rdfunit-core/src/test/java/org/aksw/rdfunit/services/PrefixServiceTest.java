e6ca0ab58b58fb6ac0688d9ea569cf9855c40a3f
Dimitris Kontokostas <jimkont@gmail.com>
2014-05-14 13:42:18 +0300

    Prefix Service test (check prefix initialization)

---
+++ rdfunit-core/src/test/java/org/aksw/rdfunit/services/PrefixServiceTest.java
@@ -0,0 +1,33 @@
+package org.aksw.rdfunit.services;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class PrefixServiceTest {
+
+    /* tests if the prefix map is setup correctly */
+    @Test
+    public void testGetPrefix() throws Exception {
+        Model prefixModel = ModelFactory.createDefaultModel();
+        try {
+            prefixModel.read(PrefixService.class.getResourceAsStream("/org/aksw/rdfunit/prefixes.ttl"), null, "TURTLE");
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot init prefixes");
+        }
+        // Update Prefix Service
+        Map<String, String> prefixes = prefixModel.getNsPrefixMap();
+        for (String id: prefixes.keySet()) {
+            // All entries should match
+            String uri = PrefixService.getPrefix(id);
+            assertEquals("All prefixed should be initialized", uri, prefixes.get(id));
+        }
+
+        // The size should be the same
+        assertEquals("mismatch in size of prefixes", prefixes.size(),PrefixService.getPrefixList().size());
+    }
+}
