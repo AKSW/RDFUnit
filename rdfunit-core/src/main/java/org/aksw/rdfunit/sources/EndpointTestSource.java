5b899d68286d694e4fb0ac955c29e6e13c2e36cd
Dimitris Kontokostas <jimkont@gmail.com>
2014-07-01 17:50:43 +0300

    put cache init code in jena-sparql-api

--- rdfunit-core/src/main/java/org/aksw/rdfunit/sources/EndpointTestSource.java
+++ rdfunit-core/src/main/java/org/aksw/rdfunit/sources/EndpointTestSource.java
@@ -1,26 +1,14 @@
 package org.aksw.rdfunit.sources;
 
 import com.hp.hpl.jena.query.Query;
-import org.aksw.jena_sparql_api.cache.core.QueryExecutionFactoryCacheEx;
-import org.aksw.jena_sparql_api.cache.extra.CacheBackend;
-import org.aksw.jena_sparql_api.cache.extra.CacheFrontend;
-import org.aksw.jena_sparql_api.cache.extra.CacheFrontendImpl;
-import org.aksw.jena_sparql_api.cache.staging.CacheBackendDao;
-import org.aksw.jena_sparql_api.cache.staging.CacheBackendDaoPostgres;
-import org.aksw.jena_sparql_api.cache.staging.CacheBackendDataSource;
+import org.aksw.jena_sparql_api.cache.h2.CacheUtilsH2;
 import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
 import org.aksw.jena_sparql_api.delay.core.QueryExecutionFactoryDelay;
 import org.aksw.jena_sparql_api.http.QueryExecutionFactoryHttp;
 import org.aksw.jena_sparql_api.limit.QueryExecutionFactoryLimit;
 import org.aksw.jena_sparql_api.pagination.core.QueryExecutionFactoryPaginated;
 import org.aksw.rdfunit.enums.TestAppliesTo;
-import org.h2.jdbcx.JdbcDataSource;
-import org.h2.tools.RunScript;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -103,42 +91,13 @@ public class EndpointTestSource extends Source {
         }
 
 
-        if (this.cacheTTL > 0 ) {
+        if (this.cacheTTL > 0) {
 
             try {
-                // Copied from
-                // https://github.com/AKSW/jena-sparql-api/blob/master/jena-sparql-api-cache-h2/src/test/java/org/aksw/jena_sparql_api/SparqlTest.java
-
-                Class.forName("org.h2.Driver");
-
-                JdbcDataSource dataSource = new JdbcDataSource();
-                dataSource.setURL("jdbc:h2:file:./cache/sparql/" + getPrefix() + ";DB_CLOSE_DELAY=-1");
-                dataSource.setUser("sa");
-                dataSource.setPassword("sa");
-
-                String schemaResourceName = "/org/aksw/jena_sparql_api/cache/cache-schema-pgsql.sql";
-                InputStream in = CacheBackendDao.class.getResourceAsStream(schemaResourceName);
-
-                if (in == null) {
-                    throw new RuntimeException("Failed to load resource: " + schemaResourceName);
-                }
-
-                InputStreamReader reader = new InputStreamReader(in);
-                Connection conn = dataSource.getConnection();
-                try {
-                    RunScript.execute(conn, reader);
-                } finally {
-                    conn.close();
-                }
-
-                CacheBackendDao dao = new CacheBackendDaoPostgres(this.cacheTTL);
-                CacheBackend cacheBackend = new CacheBackendDataSource(dataSource, dao);
-                CacheFrontend cacheFrontend = new CacheFrontendImpl(cacheBackend);
-                qef = new QueryExecutionFactoryCacheEx(qef, cacheFrontend);
+                QueryExecutionFactory _qef = CacheUtilsH2.createQueryExecutionFactory(qef, "./cache/sparql/" + getPrefix(), false, cacheTTL);
+                qef = _qef;
                 log.debug("Cache for endpoint set up: " + this.getSparqlEndpoint());
-
             } catch (Exception e) {
-                //Try to create cache, if fails continue...
                 log.debug("Could not instantiate cache for Endpoint" + this.getSparqlEndpoint(), e);
             }
         }
