78597501635fb83a20030fe62a1b52d812fa3338
Dimitris Kontokostas <jimkont@gmail.com>
2014-08-22 12:20:12 +0300

    Fix count query errors when aggregates already exist see https://issues.apache.org/jira/browse/JENA-766

--- rdfunit-core/src/main/java/org/aksw/rdfunit/tests/QueryGenerationCountFactory.java
+++ rdfunit-core/src/main/java/org/aksw/rdfunit/tests/QueryGenerationCountFactory.java
@@ -13,12 +13,28 @@ import org.aksw.rdfunit.services.PrefixNSService;
 public class QueryGenerationCountFactory implements QueryGenerationFactory {
     @Override
     public String getSparqlQueryAsString(TestCase testCase) {
-        return PrefixNSService.getSparqlPrefixDecl() +
-                " SELECT (count(DISTINCT ?resource ) AS ?total ) WHERE " + testCase.getSparqlWhere();
+        return getSparqlQuery(testCase).toString();
     }
 
     @Override
     public Query getSparqlQuery(TestCase testCase) {
-        return QueryFactory.create(getSparqlQueryAsString(testCase));
+        Query query = QueryFactory.create(PrefixNSService.getSparqlPrefixDecl() +
+                " SELECT (count(DISTINCT ?resource ) AS ?total ) WHERE " + testCase.getSparqlWhere()
+        );
+        if (!query.hasHaving()) {
+            return query;
+        }
+
+        // When we have HAVING the aggregate is calculated against the HAVING expression
+        // This way we enclose the query in a sub-select and calculate the count () correctly
+        // See https://issues.apache.org/jira/browse/JENA-766
+        query = QueryFactory.create(PrefixNSService.getSparqlPrefixDecl() +
+                " SELECT (count(DISTINCT ?resource ) AS ?total ) WHERE {" +
+                " SELECT ?resource WHERE "
+                + testCase.getSparqlWhere() +
+                "}"
+        );
+
+        return query;
     }
 }
