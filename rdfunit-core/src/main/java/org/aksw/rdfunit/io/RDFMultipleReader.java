a7eb647ec260d690dad8895c8f6f39c42ce47067
Dimitris Kontokostas <jimkont@gmail.com>
2014-08-25 10:27:20 +0300

    create multiple reader

---
+++ rdfunit-core/src/main/java/org/aksw/rdfunit/io/RDFMultipleReader.java
@@ -0,0 +1,35 @@
+package org.aksw.rdfunit.io;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import org.aksw.rdfunit.exceptions.TripleReaderException;
+
+import java.util.Collection;
+
+/**
+ * @author Dimitris Kontokostas
+ *         Description
+ * @since 11/14/13 8:51 AM
+ */
+public class RDFMultipleReader extends RDFReader {
+
+    private final Collection<RDFReader> readers;
+
+    public RDFMultipleReader(Collection<RDFReader> readers) {
+        super();
+        this.readers = readers;
+    }
+
+    @Override
+    public void read(Model model) throws TripleReaderException {
+
+        for (RDFReader r : readers) {
+            try {
+                r.read(model);
+            } catch (TripleReaderException e) {
+                throw new TripleReaderException("Cannot read from reader: " + e.getMessage());
+            }
+        }
+
+
+    }
+}
