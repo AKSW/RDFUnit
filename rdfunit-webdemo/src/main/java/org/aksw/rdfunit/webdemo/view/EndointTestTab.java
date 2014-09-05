67a5ba918c0e4d64464cfca63f6e5c4bfbfc9e68
Dimitris Kontokostas <jimkont@gmail.com>
2014-09-05 14:42:47 +0200

    draft text executor view

--- rdfunit-webdemo/src/main/java/org/aksw/rdfunit/webdemo/view/EndointTestTab.java
+++ rdfunit-webdemo/src/main/java/org/aksw/rdfunit/webdemo/view/EndointTestTab.java
@@ -1,18 +1,9 @@
 package org.aksw.rdfunit.webdemo.view;
 
 import com.vaadin.shared.ui.label.ContentMode;
-import com.vaadin.ui.*;
-import org.aksw.rdfunit.Utils.RDFUnitUtils;
-import org.aksw.rdfunit.enums.TestCaseResultStatus;
-import org.aksw.rdfunit.sources.Source;
-import org.aksw.rdfunit.tests.TestCase;
-import org.aksw.rdfunit.tests.TestSuite;
-import org.aksw.rdfunit.tests.executors.monitors.TestExecutorMonitor;
-import org.aksw.rdfunit.tests.results.AggregatedTestCaseResult;
-import org.aksw.rdfunit.tests.results.TestCaseResult;
-import org.aksw.rdfunit.webdemo.RDFUnitDemoSession;
-
-import java.io.File;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
 
 /**
  * @author Dimitris Kontokostas
@@ -28,65 +19,15 @@ public class EndointTestTab extends VerticalLayout {
 //    private final TextField endpointField = new TextField();
 //    private final TextField graphField = new TextField();
 //    private final SchemaSelectorComponent schemaSelectorWidget = new SchemaSelectorComponent();
-    private final TestExecutionView testExecutionView = new TestExecutionView();
-    private final TestGenerationView testGenerationView = new TestGenerationView();
+
 
     //    private final NativeSelect limitSelect = new NativeSelect();
     private final Button clearButton = new Button("Clear");
 
-    private final Button startTestingButton = new Button("Run tests");
-    private final Button startTestingCancelButton = new Button("Cancel");
-    private final Button resultsButton = new Button("Display Results");
 
-    private final ProgressBar testingProgress = new ProgressBar();
-    private final Label testingProgressLabel = new Label("0/0");
 
     public EndointTestTab() {
         initLayout();
-
-        //TODO move this away from here
-//        File f = VaadinSession.getCurrent().getService().getBaseDirectory();
-//        String baseDir = f.getAbsolutePath() + "/data/";
-//        try {
-//
-//            RDFUnitConfiguration dbpediaConf = RDFunitConfigurationFactory.createDBpediaConfigurationSimple(baseDir);
-//            RDFUnitConfiguration dbpediaLConf = RDFunitConfigurationFactory.createDBpediaLiveConfigurationSimple(baseDir);
-//            RDFUnitConfiguration dbpediaNLConf = RDFunitConfigurationFactory.createDBpediaNLDatasetSimple(baseDir);
-//            RDFUnitConfiguration linkedChemistry = RDFunitConfigurationFactory.createConfiguration("http://linkedchemistry.info", "http://rdf.farmbio.uu.se/chembl/sparql", Arrays.asList("http://linkedchemistry.info/chembl/"), "cheminf,cito", baseDir);
-//            RDFUnitConfiguration uriBurner = RDFunitConfigurationFactory.createConfiguration("http://linkeddata.uriburner.com", "http://linkeddata.uriburner.com/sparql/", new ArrayList<String>(), "foaf,skos,geo,dcterms,prov", baseDir);
-//            RDFUnitConfiguration bbcNature = RDFunitConfigurationFactory.createConfiguration("http://bbc.lod.openlinksw.com", "http://lod.openlinksw.com/sparql", Arrays.asList("http://www.bbc.co.uk/nature/"), "dcterms,po,wo,wlo,foaf", baseDir);
-//            RDFUnitConfiguration musicBrainz = RDFunitConfigurationFactory.createConfiguration("http://musicbrainz.lod.openlinksw.com", "http://lod.openlinksw.com/sparql", Arrays.asList("http://www.bbc.co.uk/nature/"), "ov,mo,foaf", baseDir);
-//            RDFUnitConfiguration umls = RDFunitConfigurationFactory.createConfiguration("http://umls.lod.openlinksw.com", "http://lod.openlinksw.com/sparql", Arrays.asList("http://linkedlifedata.com/resource/umls"), "dcterms,skos,owl", baseDir);
-//            RDFUnitConfiguration umbel = RDFunitConfigurationFactory.createConfiguration("http://umpel.lod.openlinksw.com", "http://lod.openlinksw.com/sparql", Arrays.asList("http://umbel.org"), "vann,skos,owl", baseDir);
-//            RDFUnitConfiguration datasw = RDFunitConfigurationFactory.createConfiguration("http://datasw.lod.openlinksw.com", "http://lod.openlinksw.com/sparql", Arrays.asList("http://data.semanticweb.org"), "cal,event,tl,dcterms,bibo,rooms,cal,skos,foaf", baseDir);
-//
-//
-//            examplesSelect.addItem(uriBurner);
-//            examplesSelect.setItemCaption(uriBurner, "Uri Burner");
-//            examplesSelect.addItem(bbcNature);
-//            examplesSelect.setItemCaption(bbcNature, "BBC Nature (LOD Cache)");
-//            examplesSelect.addItem(musicBrainz);
-//            examplesSelect.setItemCaption(musicBrainz, "MusicBrainz (LOD Cache)");
-//            examplesSelect.addItem(umls);
-//            examplesSelect.setItemCaption(umls, "LinkedLifeData UMLS (LOD Cache)");
-//            examplesSelect.addItem(umbel);
-//            examplesSelect.setItemCaption(umbel, "umbel (LOD Cache)");
-//            examplesSelect.addItem(datasw);
-//            examplesSelect.setItemCaption(datasw, "data.semanticweb.org (LOD Cache)");
-//            examplesSelect.addItem(linkedChemistry);
-//            examplesSelect.setItemCaption(linkedChemistry, "LinkedChemistry");
-//            examplesSelect.addItem(dbpediaConf);
-//            examplesSelect.setItemCaption(dbpediaConf, "DBpedia");
-//            examplesSelect.addItem(dbpediaLConf);
-//            examplesSelect.setItemCaption(dbpediaLConf, "DBpedia Live");
-//            examplesSelect.addItem(dbpediaNLConf);
-//            examplesSelect.setItemCaption(dbpediaNLConf, "DBpedia NL");
-//        } catch (UndefinedSchemaException e) {
-//            //
-//        }
-
-
-        initInteractions();
     }
 
     private void initLayout() {
@@ -109,6 +50,10 @@ public class EndointTestTab extends VerticalLayout {
         TestGenerationView testGenerationView = new TestGenerationView();
         this.addComponent(testGenerationView);
 
+        this.addComponent(new Label("<h2>4. Testing</h2>", ContentMode.HTML));
+        TestExecutionView testExecutionView = new TestExecutionView();
+        this.addComponent(testExecutionView);
+
         // Set previous / next
         dataSelectorView.setNextItem(schemaSelectorView);
 
@@ -116,220 +61,13 @@ public class EndointTestTab extends VerticalLayout {
         schemaSelectorView.setNextItem(testGenerationView);
 
         testGenerationView.setPreviousItem(schemaSelectorView);
+        testGenerationView.setNextItem(testExecutionView);
+
+        testExecutionView.setPreviousItem(testGenerationView);
         //testGenerationView.setNextItem();
 
 
-        this.addComponent(new Label("<h2>4. Testing</h2>", ContentMode.HTML));
-
-        HorizontalLayout testHeader = new HorizontalLayout();
-        this.addComponent(testHeader);
-        testHeader.addStyleName("header");
-        Label testLabel = new Label("<h2>Testing</h2>", ContentMode.HTML);
-        testHeader.addComponent(testLabel);
-        testHeader.setComponentAlignment(testLabel, Alignment.MIDDLE_LEFT);
-        testHeader.addComponent(startTestingButton);
-        testHeader.setComponentAlignment(startTestingButton, Alignment.MIDDLE_CENTER);
-        testHeader.addComponent(testingProgress);
-        testHeader.setComponentAlignment(testingProgress, Alignment.MIDDLE_CENTER);
-        testHeader.addComponent(testingProgressLabel);
-        testHeader.setComponentAlignment(testingProgressLabel, Alignment.MIDDLE_CENTER);
-        testHeader.addComponent(startTestingCancelButton);
-        testHeader.setComponentAlignment(startTestingCancelButton, Alignment.MIDDLE_CENTER);
-
-        testingProgress.setWidth("150px");
-
-        this.addComponent(this.testExecutionView);
-        //this.addComponent(this.resultsButton);
     }
 
-    private void initInteractions() {
-//        examplesSelect.addValueChangeListener(new Property.ValueChangeListener() {
-//            @Override
-//            public void valueChange(Property.ValueChangeEvent valueChangeEvent) {
-//                Property property = valueChangeEvent.getProperty();
-//                RDFUnitConfiguration configuration = (RDFUnitConfiguration) property.getValue();
-//                if (configuration != null) {
-//                    setExampleConfiguration(configuration);
-//                    RDFUnitDemoSession.setRDFUnitConfiguration(configuration);
-//                }
-//            }
-//        });
 
-        clearButton.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(Button.ClickEvent clickEvent) {
-                clearConfigurations();
-            }
-        });
-
-
-        class TestExecutorThread extends Thread {
-            @Override
-            public void run() {
-
-                //TODO make this cleaner
-                if (RDFUnitDemoSession.getRDFUnitConfiguration() != null) {
-                    Source dataset = RDFUnitDemoSession.getRDFUnitConfiguration().getTestSource();
-
-                    RDFUnitDemoSession.getTestExecutor().addTestExecutorMonitor(testExecutionView);
-                    String resultsFile = RDFUnitDemoSession.getBaseDir() + "results/" + dataset.getPrefix() + ".results.ttl";
-                    //TODO refactor this, do not use cache here
-                    File f = new File(resultsFile);
-                    try {
-                        f.delete();
-                    } catch (Exception e) {
-                        // catch
-                    }
-                    RDFUnitDemoSession.getTestExecutor().execute(dataset, RDFUnitDemoSession.getTestSuite(), 3);
-                }
-
-            }
-        }
-
-        // Clicking the button creates and runs a work thread
-        startTestingButton.addClickListener(new Button.ClickListener() {
-            public void buttonClick(Button.ClickEvent event) {
-                startTestingButton.setEnabled(false);
-                final TestExecutorThread thread = new TestExecutorThread();
-                thread.start();
-            }
-        });
-
-        RDFUnitDemoSession.getTestExecutor().addTestExecutorMonitor(new TestExecutorMonitor() {
-            private long count = 0;
-            private long totalErrors = 0;
-            private long failTest = 0;
-            private long sucessTests = 0;
-            private long timeoutTests = 0;
-            private long total = 0;
-
-            @Override
-            public void testingStarted(final Source source, final TestSuite testSuite) {
-                UI.getCurrent().access(new Runnable() {
-                    @Override
-                    public void run() {
-                        count = totalErrors = failTest = sucessTests = timeoutTests = 0;
-                        total = testSuite.size();
-
-                        startTestingCancelButton.setEnabled(true);
-                        testingProgress.setEnabled(true);
-                        testingProgress.setValue(0.0f);
-                        testingProgressLabel.setValue("0/" + total);
-
-                    }
-                });
-            }
-
-            @Override
-            public void singleTestStarted(final TestCase test) {
-                UI.getCurrent().access(new Runnable() {
-                    @Override
-                    public void run() {
-
-                    }
-                });
-            }
-
-            @Override
-            public void singleTestExecuted(final TestCase test, final TestCaseResultStatus status, final java.util.Collection<TestCaseResult> results) {
-                UI.getCurrent().access(new Runnable() {
-                    @Override
-                    public void run() {
-                        long errors = 0;
-                        TestCaseResult result = RDFUnitUtils.getFirstItemInCollection(results);
-                        if (result != null && result instanceof AggregatedTestCaseResult) {
-                            errors = ((AggregatedTestCaseResult) result).getErrorCount();
-                        }
-                        count++;
-                        totalErrors += (errors > 0 ? errors : 0);
-                        if (errors == -1)
-                            timeoutTests++;
-                        else {
-                            if (errors == 0)
-                                sucessTests++;
-                            else
-                                failTest++;
-                        }
-
-                        testingProgress.setValue((float) count / total);
-                        testingProgressLabel.setValue(count + "/" + total + " (S: " + sucessTests + " / F: " + failTest + " / T: " + timeoutTests + " / E : " + totalErrors + ")");
-
-                        if ((timeoutTests == 10 || timeoutTests == 30) && sucessTests == 0 && failTest == 0) {
-                            //Too many timeouts maybe banned
-                            Notification.show("Too many timeouts",
-                                    "Maybe the endpoint banned this IP. Try a different endpoint of try the tool from a different IP.",
-                                    Notification.Type.WARNING_MESSAGE);
-                        }
-                    }
-                });
-            }
-
-            @Override
-            public void testingFinished() {
-                UI.getCurrent().access(new Runnable() {
-                    @Override
-                    public void run() {
-                        testingProgress.setValue(1.0f);
-                        testingProgressLabel.setValue("Completed! (S: " + sucessTests + " / F:" + failTest + " / T: " + timeoutTests + " / E : " + totalErrors + ")");
-                        startTestingCancelButton.setEnabled(false);
-                    }
-                });
-            }
-        });
-
-        startTestingCancelButton.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(Button.ClickEvent clickEvent) {
-                RDFUnitDemoSession.getTestExecutor().cancel();
-            }
-        });
-
-    }
-
-    private void clearConfigurations() {
-
-//        endpointField.setValue("");
-//        graphField.setValue("");
-//        schemaSelectorWidget.setSelections(new ArrayList<SchemaSource>());
-//        examplesSelect.select(null);
-
-        testExecutionView.clearTableRowsAndHide();
-        testGenerationView.clearTableRowsAndHide();
-
-        //generateTestsProgressLabel.setValue("0/0");
-        testingProgressLabel.setValue("0/0");
-
-    }
-
-//    private void createConfigurationFromUser() {
-//
-//        String datasetURI = endpointField.getValue().replace("/sparql", "");
-//
-//        RDFUnitConfiguration configuration = new RDFUnitConfiguration(datasetURI, RDFUnitDemoSession.getBaseDir());
-//        configuration.setEndpointConfiguration(endpointField.getValue(), Arrays.asList(graphField.getValue()));
-//        configuration.setSchemata(schemaSelectorWidget.getSelections());
-//
-//        RDFUnitDemoSession.setRDFUnitConfiguration(configuration);
-//    }
-
-//    private void setExampleConfiguration(RDFUnitConfiguration configuration) {
-//        if (!(endpointField.getValue().isEmpty() || graphField.getValue().isEmpty() || schemaSelectorWidget.getSelections().isEmpty())) {
-//            //TODO confirm dialog for clear
-//            clearConfigurations();
-//        }
-//
-//        Source dataset = configuration.getTestSource();
-//        if (dataset instanceof EndpointTestSource) {
-//            endpointField.setValue(((EndpointTestSource) dataset).getSparqlEndpoint());
-//            java.util.Collection<String> graphs = ((EndpointTestSource) dataset).getSparqlGraphs();
-//            String graph = RDFUnitUtils.getFirstItemInCollection(graphs);
-//            if (graph != null && !graph.isEmpty()) {
-//                graphField.setValue(graph);
-//            }
-//        } else {
-//            endpointField.setValue("");
-//            graphField.setValue("");
-//        }
-//        schemaSelectorWidget.setSelections(dataset.getReferencesSchemata());
-//    }
 }
--- rdfunit-webdemo/src/main/java/org/aksw/rdfunit/webdemo/view/TestExecutionView.java
+++ rdfunit-webdemo/src/main/java/org/aksw/rdfunit/webdemo/view/TestExecutionView.java
@@ -1,46 +1,82 @@
 package org.aksw.rdfunit.webdemo.view;
 
-import com.google.gwt.safehtml.shared.SafeHtmlUtils;
-import com.vaadin.data.Item;
-import com.vaadin.data.Property;
-import com.vaadin.server.ExternalResource;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 import org.aksw.rdfunit.Utils.RDFUnitUtils;
 import org.aksw.rdfunit.enums.TestCaseResultStatus;
-import org.aksw.rdfunit.sources.EndpointTestSource;
 import org.aksw.rdfunit.sources.Source;
-import org.aksw.rdfunit.tests.QueryGenerationSelectFactory;
 import org.aksw.rdfunit.tests.TestCase;
 import org.aksw.rdfunit.tests.TestSuite;
+import org.aksw.rdfunit.tests.executors.TestExecutor;
+import org.aksw.rdfunit.tests.executors.monitors.SimpleTestExecutorMonitor;
 import org.aksw.rdfunit.tests.executors.monitors.TestExecutorMonitor;
 import org.aksw.rdfunit.tests.results.AggregatedTestCaseResult;
 import org.aksw.rdfunit.tests.results.TestCaseResult;
+import org.aksw.rdfunit.webdemo.RDFUnitDemoSession;
+import org.aksw.rdfunit.webdemo.utils.WorkflowUtils;
 
-import java.net.URLEncoder;
+import java.io.File;
 
 /**
  * @author Dimitris Kontokostas
  *         Description
  * @since 11/20/13 5:20 PM
  */
-class TestExecutionView extends VerticalLayout implements TestExecutorMonitor {
+class TestExecutionView extends VerticalLayout implements WorkflowItem {
+
+    private final Button startTestingButton = new Button("Run tests");
+    private final Button startTestingCancelButton = new Button("Cancel");
+    private final Button resultsButton = new Button("Display Results");
+
+    private final Label messageLabel = new Label();
+    private final TestExecutorMonitor progressMonitor = createProgressMonitor();
+    private SimpleTestExecutorMonitor modelMonitor;
+
+
+    private final ProgressBar testingProgress = new ProgressBar();
+    private final Label testingProgressLabel = new Label("0/0");
 
     private final Table resultsTable = new Table("Test Results");
-    private Source source = null;
+//    private Source source = null;
+
+
+    private WorkflowItem previous;
+    private WorkflowItem next;
+
+    private volatile boolean isReady = false;
+    private volatile boolean inProgress = false;
 
     public TestExecutionView() {
+
         initLayout();
 
     }
 
 
-    public void clearTableRowsAndHide() {
-        resultsTable.removeAllItems();
-        resultsTable.setVisible(false);
-    }
+//    public void clearTableRowsAndHide() {
+//        resultsTable.removeAllItems();
+//        resultsTable.setVisible(false);
+//    }
 
     private void initLayout() {
         this.setWidth("100%");
+
+        HorizontalLayout testHeader = new HorizontalLayout();
+        this.addComponent(testHeader);
+        testHeader.addStyleName("header");
+        Label testLabel = new Label("<h2>Testing</h2>", ContentMode.HTML);
+        testHeader.addComponent(testLabel);
+        testHeader.setComponentAlignment(testLabel, Alignment.MIDDLE_LEFT);
+        testHeader.addComponent(startTestingButton);
+        testHeader.setComponentAlignment(startTestingButton, Alignment.MIDDLE_CENTER);
+        testHeader.addComponent(testingProgress);
+        testHeader.setComponentAlignment(testingProgress, Alignment.MIDDLE_CENTER);
+        testHeader.addComponent(testingProgressLabel);
+        testHeader.setComponentAlignment(testingProgressLabel, Alignment.MIDDLE_CENTER);
+        testHeader.addComponent(startTestingCancelButton);
+        testHeader.setComponentAlignment(startTestingCancelButton, Alignment.MIDDLE_CENTER);
+
+        testingProgress.setWidth("150px");
 
         //resultsTable.setSizeFull();
         resultsTable.addContainerProperty("S", String.class, null);
@@ -53,11 +89,178 @@ class TestExecutionView extends VerticalLayout implements TestExecutorMonitor {
         this.addComponent(resultsTable);
 
 
-        //
+        // Clicking the button creates and runs a work thread
+        startTestingButton.addClickListener(new Button.ClickListener() {
+            public void buttonClick(Button.ClickEvent event) {
+                startTestingButton.setEnabled(false);
+                UI.getCurrent().push();
+                TestExecutionView.this.execute();
+            }
+        });
+
+        startTestingCancelButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                TestExecutionView.this.setMessage("Sending cancel signal, waiting for currect test to execute", false);
+                UI.getCurrent().push();
+                RDFUnitDemoSession.getTestExecutor().cancel();
+            }
+        });
+    }
+
+        //progressMonitor =
+
+    @Override
+    public void setMessage(String message, boolean isError) {
+        this.isReady = !isError;
+        WorkflowUtils.setMessage(messageLabel, message, isError);
+    }
+
+    @Override
+    public boolean isReady() {
+        return false;
+    }
+
+    @Override
+    public void setReady(boolean isReady) {
+        this.isReady = isReady;
+    }
+
+    @Override
+    public void setPreviousItem(WorkflowItem item) {
+        this.previous = item;
+    }
+
+    @Override
+    public void setNextItem(WorkflowItem item) {
+        this.next = item;
+    }
+
+    @Override
+    public WorkflowItem getPreviousItem() {
+        return previous;
+    }
+
+    @Override
+    public WorkflowItem getNextItem() {
+        return next;
+    }
+
+    @Override
+    public boolean execute() {
+
+
+        class TestExecutorThread extends Thread {
+            @Override
+            public void run() {
+
+                    modelMonitor = new SimpleTestExecutorMonitor(false);
+                    TestExecutor testExecutor = RDFUnitDemoSession.getTestExecutor();
+                    testExecutor.clearTestExecutorMonitor();
+                    testExecutor.addTestExecutorMonitor(progressMonitor);
+                    testExecutor.addTestExecutorMonitor(modelMonitor);
+
+                    Source dataset = RDFUnitDemoSession.getRDFUnitConfiguration().getTestSource();
+
+                    String resultsFile = RDFUnitDemoSession.getBaseDir() + "results/" + dataset.getPrefix() + ".results.ttl";
+                    //TODO refactor this, do not use cache here
+                    File f = new File(resultsFile);
+                    try {
+                        f.delete();
+                    } catch (Exception e) {
+                        // catch
+                    }
+                    RDFUnitDemoSession.getTestExecutor().execute(dataset, RDFUnitDemoSession.getTestSuite(), 3);
+
+            }
+        }
+
+        if (RDFUnitDemoSession.getRDFUnitConfiguration() == null)
+            return false;
+
+
+
+        final TestExecutorThread thread = new TestExecutorThread();
+        thread.start();
+        return true;
+    }
+
+    private TestExecutorMonitor createProgressMonitor() {
+        return new TestExecutorMonitor() {
+            private long count = 0;
+            private long totalErrors = 0;
+            private long failTest = 0;
+            private long sucessTests = 0;
+            private long timeoutTests = 0;
+            private long total = 0;
+
+            @Override
+            public void testingStarted(final Source source, final TestSuite testSuite) {
+
+                count = totalErrors = failTest = sucessTests = timeoutTests = 0;
+                total = testSuite.size();
+
+                startTestingCancelButton.setEnabled(true);
+                testingProgress.setEnabled(true);
+                testingProgress.setValue(0.0f);
+                testingProgressLabel.setValue("0/" + total);
+                UI.getCurrent().push();
+            }
+
+            @Override
+            public void singleTestStarted(final TestCase test) {
+
+            }
+
+            @Override
+            public void singleTestExecuted(final TestCase test, final TestCaseResultStatus status, final java.util.Collection<TestCaseResult> results) {
+
+                long errors = 0;
+                TestCaseResult result = RDFUnitUtils.getFirstItemInCollection(results);
+                if (result != null && result instanceof AggregatedTestCaseResult) {
+                    errors = ((AggregatedTestCaseResult) result).getErrorCount();
+                }
+                count++;
+                totalErrors += (errors > 0 ? errors : 0);
+                if (errors == -1)
+                    timeoutTests++;
+                else {
+                    if (errors == 0)
+                        sucessTests++;
+                    else
+                        failTest++;
+                }
+
+                testingProgress.setValue((float) count / total);
+                testingProgressLabel.setValue(count + "/" + total + " (S: " + sucessTests + " / F: " + failTest + " / T: " + timeoutTests + " / E : " + totalErrors + ")");
+
+                if ((timeoutTests == 10 || timeoutTests == 30) && sucessTests == 0 && failTest == 0) {
+                    //Too many timeouts maybe banned
+                    Notification.show("Too many timeouts",
+                            "Maybe the endpoint banned this IP. Try a different endpoint of try the tool from a different IP.",
+                            Notification.Type.WARNING_MESSAGE);
+                }
+
+                UI.getCurrent().push();
+            }
+
+            @Override
+            public void testingFinished() {
+
+                testingProgress.setValue(1.0f);
+                setMessage("Completed! (S: " + sucessTests + " / F:" + failTest + " / T: " + timeoutTests + " / E : " + totalErrors + ")", false);
+                startTestingCancelButton.setEnabled(true);
+                UI.getCurrent().push();
+            }
+        };
 
 
     }
 
+
+
+
+     /*
     @Override
     public void testingStarted(final Source source, final TestSuite testSuite) {
         this.source = source;
@@ -154,4 +357,6 @@ class TestExecutionView extends VerticalLayout implements TestExecutorMonitor {
             }
         });
     }
+    */
+
 }
