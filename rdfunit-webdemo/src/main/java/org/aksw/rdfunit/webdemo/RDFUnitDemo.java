9d306b4a93c0a63d0840ccbae4087174c5197190
Dimitris Kontokostas <jimkont@gmail.com>
2014-09-12 13:08:32 +0300

    get ip address even behind a proxy

--- rdfunit-webdemo/src/main/java/org/aksw/rdfunit/webdemo/RDFUnitDemo.java
+++ rdfunit-webdemo/src/main/java/org/aksw/rdfunit/webdemo/RDFUnitDemo.java
@@ -47,8 +47,7 @@ public class RDFUnitDemo extends UI {
     protected void init(VaadinRequest request) {
 
 
-
-        RDFUnitDemoSession.init("http://" + request.getRemoteHost());
+        RDFUnitDemoSession.init("http://" + getClientIpAddr(request));
 
         initLayout();
         setPollInterval(15000);
@@ -116,6 +115,26 @@ public class RDFUnitDemo extends UI {
         layoutFooter.setHeight("40px");
         layoutFooter.addStyleName("v-link");
         layoutFooter.addComponent(new Label("@ <a href=\"http://aksw.org\">AKSW</a> / <a href=\"http://rdfunit.aksw.org\">RDFUnit</a>", ContentMode.HTML));
+    }
+
+    private String getClientIpAddr(VaadinRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 
 }
