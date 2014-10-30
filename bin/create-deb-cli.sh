04a68e6ccdce69fdf5bd303f12fb6ec76bf4d54d
Dimitris Kontokostas <jimkont@gmail.com>
2014-10-30 22:19:21 +0200

    fix deb creation script / create new debian for web demo #25

--- bin/create-deb-cli.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-comment=$(git log -1)
-version=$(mvn help:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
-cd rdfunit-validate
-dch -v $version "$comment"
-comment="$(echo "$comment"|tr -d '\n')"
-sed -i "1s/^/ * $comment\n/" debian/CHANGES.txt
-mvn clean install -DskipTests -Pdebpackage -am
