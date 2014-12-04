c7f4edff48e1cd07a6900cf03efb6741f0987296
Dimitris Kontokostas <jimkont@gmail.com>
2014-12-04 12:02:57 +0200

    auto-import optimization

--- rdfunit-core/src/test/java/org/aksw/rdfunit/enums/PatternParameterConstraintsTest.java
+++ rdfunit-core/src/test/java/org/aksw/rdfunit/enums/PatternParameterConstraintsTest.java
@@ -4,7 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PatternParameterConstraintsTest {
 
