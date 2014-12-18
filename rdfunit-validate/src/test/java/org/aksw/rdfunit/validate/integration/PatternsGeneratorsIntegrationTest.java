db145516454c3c1ef2032908f93232eec36add8a
Dimitris Kontokostas <jimkont@gmail.com>
2014-12-18 18:18:31 +0200

    check if object values are actual IRIs

--- rdfunit-validate/src/test/java/org/aksw/rdfunit/validate/integration/PatternsGeneratorsIntegrationTest.java
+++ rdfunit-validate/src/test/java/org/aksw/rdfunit/validate/integration/PatternsGeneratorsIntegrationTest.java
@@ -61,6 +61,8 @@ public class PatternsGeneratorsIntegrationTest {
         testsWithErrorsOWL.put("owl/OWLQCARDT_Wrong_Max.ttl", 2);
         testsWithErrorsOWL.put("owl/RDFLANGSTRING_Correct.ttl", 0);
         testsWithErrorsOWL.put("owl/RDFLANGSTRING_Wrong.ttl", 2);
+        testsWithErrorsOWL.put("owl/RDFSRANG_LIT_Correct.ttl", 0);
+        testsWithErrorsOWL.put("owl/RDFSRANG_LIT_Wrong.ttl", 4);
 
         RDFReader ontologyOWLReader = RDFReaderFactory.createResourceReader(resourcePrefix + "owl/ontology.ttl");
         SchemaSource ontologyOWLSource = new SchemaSource("tests", "http://rdfunit.aksw.org", ontologyOWLReader);
