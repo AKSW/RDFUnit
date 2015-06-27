a2f9cdad189709223393127f9018213faf07d1a3
Dimitris Kontokostas <jimkont@gmail.com>
2015-06-27 19:24:17 +0200

    refactor statistics & namespace generation

---
+++ rdfunit-core/src/main/java/org/aksw/rdfunit/statistics/DatasetStatisticsClasses.java
@@ -0,0 +1,27 @@
+package org.aksw.rdfunit.statistics;
+
+import org.aksw.rdfunit.utils.PrefixNSService;
+
+/**
+ * Description
+ *
+ * @author Dimitris Kontokostas
+ * @since 6/27/15 1:02 PM
+ */
+public class DatasetStatisticsClasses extends DatasetStatistics {
+
+
+    /*
+    * Simple SPARQL query to get all class occurrences
+    * */
+    private static final String classStatsSPARQL = PrefixNSService.getSparqlPrefixDecl() +
+            " SELECT DISTINCT ?stats WHERE {" +
+            "     ?s a ?stats . } ";
+
+    @Override
+    public String getStatisticsQuery() {
+        return classStatsSPARQL;
+    }
+
+
+}
---
+++ rdfunit-core/src/test/java/org/aksw/rdfunit/statistics/DatasetStatisticsPropertiesTest.java
@@ -0,0 +1,36 @@
+package org.aksw.rdfunit.statistics;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Description
+ *
+ * @author Dimitris Kontokostas
+ * @since 6/27/15 6:45 PM
+ */
+public class DatasetStatisticsPropertiesTest extends DatasetStatisticsTest {
+
+
+    private static final int EXPECTED_ITEMS = 6;
+
+    @Override
+    protected int getExteptedItems() {
+        return EXPECTED_ITEMS;
+    }
+
+    @Override
+    protected DatasetStatistics getStatisticsObject() {
+        return new DatasetStatisticsProperties();
+    }
+
+    @Test
+    public void testGetStats() throws Exception {
+        for (Map.Entry<String, Integer> entry : executeBasicTest().entrySet()) {
+            assertEquals(new Integer(0), entry.getValue());
+        }
+    }
+}
