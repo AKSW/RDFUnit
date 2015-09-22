d23171258438551266439b364adbc029911cd593
Daniel Hiller <dhiller@wolterskluwer.de>
2015-09-22 09:10:03 +0200

    Issue #49 - refactoring of test runner

--- rdfunit-junit/src/main/java/org/aksw/rdfunit/junit/RdfUnitJunitRunner.java
+++ rdfunit-junit/src/main/java/org/aksw/rdfunit/junit/RdfUnitJunitRunner.java
@@ -1,6 +1,8 @@
 package org.aksw.rdfunit.junit;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
@@ -24,6 +26,96 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class RdfUnitJunitRunner extends ParentRunner<RdfUnitJunitRunner.RdfUnitJunitTestCase> {
 
+    private final List<RdfUnitJunitRunner.RdfUnitJunitTestCase> testCases = new ArrayList<>();
+
+    public RdfUnitJunitRunner(Class<?> testClass) throws InitializationError {
+        super(testClass);
+
+        checkOntologyAnnotation();
+        checkInputModelAnnotatedMethods();
+
+        generateRdfUnitTestCases();
+    }
+
+    private void checkOntologyAnnotation() throws InitializationError {
+        if (getTestClass().getJavaClass().isAnnotationPresent(Ontology.class)) {
+            return;
+        }
+        throw new InitializationError("@Ontology annotation is required!");
+    }
+
+    private void checkInputModelAnnotatedMethods() throws InitializationError {
+        List<FrameworkMethod> inputModelMethods = getTestClass().getAnnotatedMethods(InputModel.class);
+        if (inputModelMethods.isEmpty()) {
+            throw new InitializationError("At least one method with @InputModel annotation is required!");
+        }
+
+        for (FrameworkMethod m : inputModelMethods) {
+            if (!m.getReturnType().equals(Model.class)) {
+                throw new InitializationError("Methods marked @InputModel must return com.hp.hpl.jena.rdf.model.Model");
+            }
+        }
+    }
+
+    private void generateRdfUnitTestCases() throws InitializationError {
+        final List<FrameworkMethod> annotatedMethods = getTestClass().getAnnotatedMethods(InputModel.class);
+        for (TestCase t : createTestCases()) {
+            for (FrameworkMethod m : annotatedMethods) {
+                testCases.add(new RdfUnitJunitTestCase(m, t));
+            }
+        }
+    }
+
+    private Collection<TestCase> createTestCases() throws InitializationError {
+        final RDFReader ontologyReader = getRdfReaderForOntology();
+        return new RDFUnitTestSuiteGenerator.Builder()
+                .addSchemaURI("custom", getOntology().uri(), ontologyReader)
+                .enableAutotests()
+                .build().getTestSuite().getTestCases();
+    }
+
+    private RDFReader getRdfReaderForOntology() throws InitializationError {
+        final RDFReader ontologyReader;
+
+        try (final InputStream in = getOntologyUrl().openStream()) {
+            ontologyReader = new RDFModelReader(ModelFactory.createDefaultModel().read(in, getOntology
+                    ()
+                    .format()));
+        } catch (IOException e) {
+            throw new InitializationError(e);
+        }
+        return ontologyReader;
+    }
+
+    private URL getOntologyUrl() throws InitializationError {
+        final URL url;
+        try {
+            url = URI.create(getOntology().uri()).toURL();
+        } catch (MalformedURLException e) {
+            throw new InitializationError(e);
+        }
+        return url;
+    }
+
+    private Ontology getOntology() {
+        return getTestClass().getAnnotation(Ontology.class);
+    }
+
+    @Override
+    protected List<RdfUnitJunitRunner.RdfUnitJunitTestCase> getChildren() {
+        return Collections.unmodifiableList(testCases);
+    }
+
+    @Override
+    protected Description describeChild(RdfUnitJunitRunner.RdfUnitJunitTestCase child) {
+        return Description.createTestDescription(this.getTestClass().getJavaClass(), "RdfUnitJunitRunner");
+    }
+
+    @Override
+    protected void runChild(RdfUnitJunitRunner.RdfUnitJunitTestCase child, RunNotifier notifier) {
+
+    }
+
     public static final class RdfUnitJunitTestCase {
         private final FrameworkMethod frameworkMethod;
         private final TestCase testCase;
@@ -40,66 +132,6 @@ public class RdfUnitJunitRunner extends ParentRunner<RdfUnitJunitRunner.RdfUnitJ
         public TestCase getTestCase() {
             return testCase;
         }
-    }
-
-    private final List<RdfUnitJunitRunner.RdfUnitJunitTestCase> testCases = new ArrayList<>();
-
-    public RdfUnitJunitRunner(Class<?> testClass) throws InitializationError {
-        super(testClass);
-
-        if (!testClass.isAnnotationPresent(Ontology.class)) {
-            throw new InitializationError("@Ontology annotation is required!");
-        }
-
-        checkAnnotatedMethods();
-
-        final Ontology ontology = testClass.getAnnotation(Ontology.class);
-        final RDFReader ontologyReader;
-        try {
-            final URL url = URI.create(ontology.uri()).toURL();
-            ontologyReader = new RDFModelReader(ModelFactory.createDefaultModel().read(url.openStream(), ontology
-                    .format()));
-        } catch (IOException e) {
-            throw new InitializationError(e);
-        }
-        final Collection<TestCase> testCaseCollection = new RDFUnitTestSuiteGenerator.Builder()
-                .addSchemaURI("custom", ontology.uri(), ontologyReader)
-                .enableAutotests()
-                .build().getTestSuite().getTestCases();
-        final List<FrameworkMethod> annotatedMethods = getTestClass().getAnnotatedMethods(InputModel.class);
-        for (TestCase t : testCaseCollection) {
-            for (FrameworkMethod m : annotatedMethods) {
-                testCases.add(new RdfUnitJunitTestCase(m, t));
-            }
-        }
-    }
-
-    private void checkAnnotatedMethods() throws InitializationError {
-        List<FrameworkMethod> inputModelMethods = getTestClass().getAnnotatedMethods(InputModel.class);
-        if(inputModelMethods.isEmpty()) {
-            throw new InitializationError("At least one method with @InputModel annotation is required!");
-        }
-
-        for(FrameworkMethod m : inputModelMethods) {
-            if(!m.getReturnType().equals(Model.class)) {
-                throw new InitializationError("Methods marked @InputModel must return com.hp.hpl.jena.rdf.model.Model");
-            }
-        }
-    }
-
-    @Override
-    protected List<RdfUnitJunitRunner.RdfUnitJunitTestCase> getChildren() {
-        return Collections.unmodifiableList(testCases);
-    }
-
-    @Override
-    protected Description describeChild(RdfUnitJunitRunner.RdfUnitJunitTestCase child) {
-        return Description.createTestDescription(this.getTestClass().getJavaClass(), "RdfUnitJunitRunner");
-    }
-
-    @Override
-    protected void runChild(RdfUnitJunitRunner.RdfUnitJunitTestCase child, RunNotifier notifier) {
-
     }
 
 }
