f621536255b744f6ca583c8c56432a1f47eea935
Dimitris Kontokostas <jimkont@gmail.com>
2015-09-29 08:53:30 +0300

    Test generator complete tests

---
+++ rdfunit-core/src/main/java/org/aksw/rdfunit/model/writers/TestGeneratorWriter.java
@@ -0,0 +1,47 @@
+package org.aksw.rdfunit.model.writers;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.DCTerms;
+import com.hp.hpl.jena.vocabulary.RDF;
+import org.aksw.rdfunit.model.interfaces.ResultAnnotation;
+import org.aksw.rdfunit.model.interfaces.TestGenerator;
+import org.aksw.rdfunit.vocabulary.RDFUNITv;
+
+/**
+ * Description
+ *
+ * @author Dimitris Kontokostas
+ * @since 6/17/15 5:57 PM
+ * @version $Id: $Id
+ */
+public final class TestGeneratorWriter implements ElementWriter {
+
+    private final TestGenerator testGenerator;
+
+    private TestGeneratorWriter(TestGenerator testGenerator) {
+        this.testGenerator = testGenerator;
+    }
+
+    public static TestGeneratorWriter create(TestGenerator testGenerator) {return new TestGeneratorWriter(testGenerator);}
+
+    /** {@inheritDoc} */
+    @Override
+    public Resource write(Model model) {
+        Resource resource = ElementWriterUtils.copyElementResourceInModel(testGenerator, model);
+
+        resource
+                .addProperty(RDF.type, RDFUNITv.TestGenerator)
+                .addProperty(DCTerms.description, testGenerator.getTAGDescription())
+                .addProperty(RDFUNITv.sparqlGenerator, testGenerator.getTAGQuery())
+                .addProperty(RDFUNITv.basedOnPattern, ElementWriterUtils.copyElementResourceInModel(testGenerator.getTAGPattern(), model));
+
+
+        for (ResultAnnotation resultAnnotation: testGenerator.getTAGAnnotations()) {
+            Resource annotationResource = ResultAnnotationWriter.create(resultAnnotation).write(model);
+            resource.addProperty(RDFUNITv.resultAnnotation, annotationResource);
+        }
+
+        return resource;
+    }
+}
---
+++ rdfunit-core/src/test/java/org/aksw/rdfunit/model/writers/TestGeneratorWriterTest.java
@@ -0,0 +1,68 @@
+package org.aksw.rdfunit.model.writers;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import org.aksw.rdfunit.RDFUnit;
+import org.aksw.rdfunit.Resources;
+import org.aksw.rdfunit.io.reader.RDFReaderFactory;
+import org.aksw.rdfunit.model.interfaces.TestGenerator;
+import org.aksw.rdfunit.model.readers.TestGeneratorBatchReader;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Description
+ *
+ * @author Dimitris Kontokostas
+ * @since 9/29/15 8:41 AM
+ */
+@RunWith(Parameterized.class)
+public class TestGeneratorWriterTest {
+
+    @Before
+    public void setUp() throws Exception {
+        // Needed to resolve the patterns
+        RDFUnit rdfUnit = new RDFUnit();
+        rdfUnit.init();
+    }
+
+
+    @Parameterized.Parameters(name= "{index}: Model: {1} ")
+    public static Collection<Object[]> models() throws Exception {
+        return Arrays.asList( new Object[][]{
+                        {RDFReaderFactory.createResourceReader(Resources.AUTO_GENERATORS_OWL).read(), "OWLGen"},
+                        {RDFReaderFactory.createResourceReader(Resources.AUTO_GENERATORS_RS).read(), "RSGen"},
+                        {RDFReaderFactory.createResourceReader(Resources.AUTO_GENERATORS_DSP).read(), "DSPGen"}
+                });
+    }
+
+    @Parameterized.Parameter
+    public Model inputModel;
+    @Parameterized.Parameter(value=1)
+    public String label;
+
+    @Test
+    public void testWrite() throws Exception {
+        Collection<TestGenerator> testCaseCollection = TestGeneratorBatchReader.create().getTestGeneratorsFromModel(inputModel);
+
+        Model modelWritten = ModelFactory.createDefaultModel();
+        for (TestGenerator tg : testCaseCollection) {
+            TestGeneratorWriter.create(tg).write(modelWritten);
+        }
+
+        // See the difference...
+        //Model difference = inputModel.difference(modelWritten);
+        //new RDFFileWriter("tmp" + label.replace("/", "_") + ".in.ttl", "TTL").write(inputModel);
+        //new RDFFileWriter("tmp" + label.replace("/", "_") + ".out.ttl", "TTL").write(modelWritten);
+        //new RDFFileWriter("tmp" + label.replace("/", "_") + ".diff.ttl", "TTL").write(difference);
+
+        assertThat(inputModel.isIsomorphicWith(modelWritten)).isTrue();
+    }
+}
