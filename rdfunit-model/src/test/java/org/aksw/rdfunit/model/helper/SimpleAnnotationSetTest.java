0ea89805c0a966cc8712642e00b3a12bf093dca7
Dimitris Kontokostas <jimkont@gmail.com>
2016-01-14 09:43:20 +0200

    some extra testing

---
+++ rdfunit-model/src/test/java/org/aksw/rdfunit/model/helper/SimpleAnnotationSetTest.java
@@ -0,0 +1,55 @@
+package org.aksw.rdfunit.model.helper;
+
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * Description
+ *
+ * @author Dimitris Kontokostas
+ * @since 14/1/2016 9:30 πμ
+ */
+public class SimpleAnnotationSetTest {
+
+    @Test
+    public void testGetAnnotations() throws Exception {
+
+        //add rdf:type rdfs:Class
+        SimpleAnnotationSet an1 = SimpleAnnotationSet.create();
+        an1.add(RDF.type, RDFS.Class);
+        assertThat(an1.getAnnotations().size())
+                .isEqualTo(1);
+        List<SimpleAnnotation> annotations = new ArrayList<>(an1.getAnnotations());
+        assertThat(annotations.get(0).getProperty())
+                .isEqualTo(RDF.type);
+        assertThat(annotations.get(0).getValues().size())
+                .isEqualTo(1);
+        assertThat(((new ArrayList<RDFNode>(annotations.get(0).getValues())).get(0)))
+                .isEqualTo(RDFS.Class);
+
+        //add rdf:type rdfs:Literal
+        an1.add(RDF.type, RDFS.Literal);
+        assertThat(an1.getAnnotations().size())
+                .isEqualTo(1);
+        annotations = new ArrayList<>(an1.getAnnotations());
+        assertThat(annotations.get(0).getProperty())
+                .isEqualTo(RDF.type);
+        assertThat(annotations.get(0).getValues().size())
+                .isEqualTo(2);
+
+        //add rdf:subject rdfs:Datatype
+        an1.add(RDF.subject, RDFS.Datatype);
+        assertThat(an1.getAnnotations().size())
+                .isEqualTo(2);
+
+
+    }
+}
