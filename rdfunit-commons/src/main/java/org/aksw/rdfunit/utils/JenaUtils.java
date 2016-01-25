fed4f26cbaf7cab65b53a5c0650ffa40afe760cf
Dimitris Kontokostas <jimkont@gmail.com>
2016-01-26 01:57:59 +0200

    minors

--- rdfunit-commons/src/main/java/org/aksw/rdfunit/utils/JenaUtils.java
+++ rdfunit-commons/src/main/java/org/aksw/rdfunit/utils/JenaUtils.java
@@ -4,12 +4,14 @@ import com.hp.hpl.jena.shared.uuid.JenaUUID;
 
 public final class JenaUtils {
 
+    private static final String DEFAULT_PREFIX = "urn:uuid:";
+
     public static String getUniqueIri(String prefix) {
         return prefix + JenaUUID.generate().asString();
     }
 
     public static String getUniqueIri() {
-        return getUniqueIri("urn:uuid:");
+        return getUniqueIri(JenaUtils.DEFAULT_PREFIX);
     }
 
 }
