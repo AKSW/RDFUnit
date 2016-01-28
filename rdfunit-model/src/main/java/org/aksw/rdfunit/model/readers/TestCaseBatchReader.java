eb48e1a01816e902f0456df70394377ec98b1452
Dimitris Kontokostas <jimkont@gmail.com>
2016-01-28 21:47:22 +0200

    avoid concurrency / race conditions issues

--- rdfunit-model/src/main/java/org/aksw/rdfunit/model/readers/TestCaseBatchReader.java
+++ rdfunit-model/src/main/java/org/aksw/rdfunit/model/readers/TestCaseBatchReader.java
@@ -1,15 +1,15 @@
 package org.aksw.rdfunit.model.readers;
 
+import com.google.common.collect.ImmutableList;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.RDF;
 import org.aksw.rdfunit.model.interfaces.TestCase;
 import org.aksw.rdfunit.vocabulary.RDFUNITv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Reader a set of Patterns
@@ -38,21 +38,27 @@ public final class TestCaseBatchReader {
      * @return a {@link java.util.Collection} object.
      */
     public Collection<TestCase> getTestCasesFromModel(Model model) {
-        Collection<TestCase> testCases = new ArrayList<>();
+        ConcurrentLinkedQueue<TestCase> testCases = new ConcurrentLinkedQueue<>();
 
-        for (Resource resource: model.listResourcesWithProperty(RDF.type, RDFUNITv.ManualTestCase).toList()){
-            testCases.add(ManualTestCaseReader.create().read(resource));
-        }
+        model.listResourcesWithProperty(RDF.type, RDFUNITv.ManualTestCase).toList()
+                .parallelStream()
+                .forEach(resource -> {
+                    testCases.add(ManualTestCaseReader.create().read(resource));
+                });
 
-        for (Resource resource: model.listResourcesWithProperty(RDF.type, RDFUNITv.PatternBasedTestCase).toList()){
-            try {
-                testCases.add(PatternBasedTestCaseReader.create().read(resource));
-            } catch (IllegalArgumentException ex) {
-                log.warn("Cannot create PatternBasedTestCase {}", resource.toString(), ex);
-            }
-        }
 
-        return testCases;
+        model.listResourcesWithProperty(RDF.type, RDFUNITv.PatternBasedTestCase)
+                .toList()
+                .parallelStream()
+                .forEach(resource -> {
+                    try {
+                        testCases.add(PatternBasedTestCaseReader.create().read(resource));
+                    } catch (IllegalArgumentException ex) {
+                        log.warn("Cannot create PatternBasedTestCase {}", resource.toString(), ex);
+                    }
+                });
+
+        return ImmutableList.copyOf(testCases);
 
     }
 
