4b0825e095d9bca6def9e806aee5f209bdcc0376
Sandro Athaide Coelho <sandroacoelho@gmail.com>
2016-02-23 07:18:08 -0300

    Fix #63 - rebase with master

--- rdfunit-core/src/test/java/org/aksw/rdfunit/validate/wrappers/RDFUnitStaticValidatorTest.java
+++ rdfunit-core/src/test/java/org/aksw/rdfunit/validate/wrappers/RDFUnitStaticValidatorTest.java
@@ -2,8 +2,9 @@ package org.aksw.rdfunit.validate.wrappers;
 
 
 import org.aksw.rdfunit.enums.TestCaseExecutionType;
-import org.aksw.rdfunit.io.reader.RDFReaderException;
-import org.aksw.rdfunit.io.reader.RDFReaderFactory;
+
+import org.aksw.rdfunit.io.reader.RdfReaderException;
+import org.aksw.rdfunit.io.reader.RdfReaderFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
 import org.junit.Test;
@@ -11,12 +12,12 @@ import org.junit.Test;
 public class RDFUnitStaticValidatorTest {
 
     @Test
-    public void testValidateModelShaclFullTestCaseResultMode() throws RDFReaderException {
+    public void testValidateModelShaclFullTestCaseResultMode() throws RdfReaderException {
         //Init
         String turtle = "";
 
         //Act
-        Model model = RDFReaderFactory.createReaderFromText(turtle, Lang.NTRIPLES.getName()).read();
+        Model model = RdfReaderFactory.createReaderFromText(turtle, Lang.NTRIPLES.getName()).read();
 
         RDFUnitStaticValidator.initWrapper(
                 new RDFUnitTestSuiteGenerator.Builder()
@@ -28,12 +29,12 @@ public class RDFUnitStaticValidatorTest {
     }
 
     @Test
-    public void testValidateModelStatusTestCaseResultMode() throws RDFReaderException {
+    public void testValidateModelStatusTestCaseResultMode() throws RdfReaderException {
         //Init
         String turtle = "";
 
         //Act
-        Model model = RDFReaderFactory.createReaderFromText(turtle, Lang.NTRIPLES.getName()).read();
+        Model model = RdfReaderFactory.createReaderFromText(turtle, Lang.NTRIPLES.getName()).read();
 
         RDFUnitStaticValidator.initWrapper(
                 new RDFUnitTestSuiteGenerator.Builder()
@@ -46,12 +47,12 @@ public class RDFUnitStaticValidatorTest {
 
 
     @Test
-    public void testValidateModelAggregatedTestCaseResultMode() throws RDFReaderException {
+    public void testValidateModelAggregatedTestCaseResultMode() throws RdfReaderException {
         //Init
         String turtle = "";
 
         //Act
-        Model model = RDFReaderFactory.createReaderFromText(turtle, Lang.NTRIPLES.getName()).read();
+        Model model = RdfReaderFactory.createReaderFromText(turtle, Lang.NTRIPLES.getName()).read();
 
         RDFUnitStaticValidator.initWrapper(
                 new RDFUnitTestSuiteGenerator.Builder()
@@ -63,12 +64,12 @@ public class RDFUnitStaticValidatorTest {
     }
 
     @Test
-    public void testValidateModelShaclSimpleTestCaseResultMode() throws RDFReaderException {
+    public void testValidateModelShaclSimpleTestCaseResultMode() throws RdfReaderException {
         //Init
         String turtle = "";
 
         //Act
-        Model model = RDFReaderFactory.createReaderFromText(turtle, Lang.NTRIPLES.getName()).read();
+        Model model = RdfReaderFactory.createReaderFromText(turtle, Lang.NTRIPLES.getName()).read();
 
         RDFUnitStaticValidator.initWrapper(
                 new RDFUnitTestSuiteGenerator.Builder()
