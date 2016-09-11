7dd5991734fb2564eba651b93def94b512531679
Dimitris Kontokostas <jimkont@gmail.com>
2016-09-11 18:15:58 +0200

    example JUnit / RDFUnit test with SHACL

---
+++ rdfunit-examples/src/test/java/org/aksw/rdfunit/examples/JunitShaclTest.java
@@ -0,0 +1,25 @@
+package org.aksw.rdfunit.examples;
+
+import org.aksw.rdfunit.io.reader.RdfModelReader;
+import org.aksw.rdfunit.io.reader.RdfReader;
+import org.aksw.rdfunit.io.reader.RdfReaderException;
+import org.aksw.rdfunit.io.reader.RdfReaderFactory;
+import org.aksw.rdfunit.junit.RdfUnitJunitRunner;
+import org.aksw.rdfunit.junit.Schema;
+import org.aksw.rdfunit.junit.TestInput;
+import org.junit.runner.RunWith;
+
+
+@RunWith(RdfUnitJunitRunner.class)
+@Schema(uri = "/org/aksw/rdfunit/examples/shacl.example.shapes.ttl")
+public class JunitShaclTest {
+
+
+    @TestInput
+    public RdfReader getInputData() throws RdfReaderException {
+        return new RdfModelReader(
+                RdfReaderFactory.createResourceReader(
+                        "/org/aksw/rdfunit/examples/shacl.example.data.ttl").read());
+    }
+
+}
