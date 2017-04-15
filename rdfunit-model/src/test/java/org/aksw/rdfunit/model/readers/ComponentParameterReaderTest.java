ce8f2e9fc5bc6a1dceac16c8f871793fcb6ff0e6
¨Dimitris <d.kontokostas@geophy.com>
2017-04-16 01:51:52 +0300

    resource rearrangements + component parameter tests

--- rdfunit-model/src/test/java/org/aksw/rdfunit/model/readers/ComponentParameterReaderTest.java
+++ rdfunit-model/src/test/java/org/aksw/rdfunit/model/readers/ComponentParameterReaderTest.java
@@ -1,13 +1,10 @@
-package org.aksw.rdfunit.model.writers;
+package org.aksw.rdfunit.model.readers;
 
 import org.aksw.rdfunit.Resources;
 import org.aksw.rdfunit.io.reader.RdfReaderException;
 import org.aksw.rdfunit.io.reader.RdfReaderFactory;
-import org.aksw.rdfunit.model.interfaces.ComponentParameter;
-import org.aksw.rdfunit.model.readers.ComponentParameterReader;
 import org.aksw.rdfunit.vocabulary.SHACL;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.junit.Test;
@@ -17,18 +14,8 @@ import org.junit.runners.Parameterized;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-
-/**
- * Description
- *
- * @author Dimitris Kontokostas
- * @since 8/21/15 12:42 AM
- */
 @RunWith(Parameterized.class)
-public class ComponentParameterWriterTest {
-
+public class ComponentParameterReaderTest {
     @Parameterized.Parameters(name= "{index}: Pattern: {0}")
     public static Collection<Object[]> resources() throws RdfReaderException {
         Model model = RdfReaderFactory.createResourceReader(Resources.SHACL_CORE_CCs).read();
@@ -42,21 +29,8 @@ public class ComponentParameterWriterTest {
     @Parameterized.Parameter
     public Resource resource;
 
-
     @Test
     public void testRead() {
-        // read the ComponentParameter
-        ComponentParameter componentParameter = ComponentParameterReader.create().read(resource);
-
-        // write in new model
-        Model m1 = ModelFactory.createDefaultModel();
-        Resource r1 = ComponentParameterWriter.create(componentParameter).write(m1);
-        // reread
-        ComponentParameter componentParameter2 = ComponentParameterReader.create().read(r1);
-        Model m2 = ModelFactory.createDefaultModel();
-        ComponentParameterWriter.create(componentParameter2).write(m2);
-
-        assertThat(m1.isIsomorphicWith(m2))
-            .isTrue();
+        ComponentParameterReader.create().read(resource);
     }
 }
