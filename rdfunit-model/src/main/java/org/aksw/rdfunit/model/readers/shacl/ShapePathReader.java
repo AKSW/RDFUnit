c83dbe201e4cd4cae9f07a332a90f3ca60e93a57
¨Dimitris <d.kontokostas@geophy.com>
2017-07-20 01:33:12 +0300

    sequence shacl path support

--- rdfunit-model/src/main/java/org/aksw/rdfunit/model/readers/shacl/ShapePathReader.java
+++ rdfunit-model/src/main/java/org/aksw/rdfunit/model/readers/shacl/ShapePathReader.java
@@ -1,10 +1,14 @@
 package org.aksw.rdfunit.model.readers.shacl;
 
+import org.aksw.rdfunit.model.helper.RdfListUtils;
 import org.aksw.rdfunit.model.impl.shacl.ShapePathImpl;
 import org.aksw.rdfunit.model.interfaces.shacl.ShapePath;
 import org.aksw.rdfunit.model.readers.ElementReader;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.sparql.path.P_Link;
+import org.apache.jena.sparql.path.Path;
+import org.apache.jena.sparql.path.PathFactory;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -25,13 +29,25 @@ public final class ShapePathReader implements ElementReader<ShapePath> {
 
         shapePathBuilder.element(resource);
 
-        if (resource.isResource()) {
-            shapePathBuilder.jenaPath(new P_Link(resource.asNode()));
-        } else {
-            throw new IllegalArgumentException("Complax paths not yet supported");
-        }
+        shapePathBuilder.jenaPath(readPath(resource));
 
         return shapePathBuilder.build();
+    }
+
+    private static Path readPath(Resource resource) {
+        if (resource.isURIResource()) {
+            return new P_Link(resource.asNode());
+        }
+
+        if (RdfListUtils.isList(resource)) {
+            return RdfListUtils.getListItemsOrEmpty(resource).stream()
+                    .filter(RDFNode::isResource)
+                    .map(RDFNode::asResource)
+                    .map(ShapePathReader::readPath)
+                    .reduce(PathFactory::pathSeq)
+                    .orElseThrow(() -> new IllegalArgumentException("Sequence path invalid"));
+        }
+        throw new IllegalArgumentException("Complex paths not yet supported");
     }
 }
 
