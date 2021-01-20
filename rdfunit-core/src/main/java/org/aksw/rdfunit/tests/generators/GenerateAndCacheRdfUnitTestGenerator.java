56b9292eb2b05692a0c0d0d8c3bf4a66ca69ecd8
mknuth <magnus.knuth@eccenca.com>
2021-01-20 21:13:46 +0100

    use vann:preferredNamespacePrefix if available in schema (BP)

--- rdfunit-core/src/main/java/org/aksw/rdfunit/tests/generators/GenerateAndCacheRdfUnitTestGenerator.java
+++ rdfunit-core/src/main/java/org/aksw/rdfunit/tests/generators/GenerateAndCacheRdfUnitTestGenerator.java
@@ -7,6 +7,7 @@ import org.aksw.rdfunit.io.writer.RdfFileWriter;
 import org.aksw.rdfunit.model.interfaces.GenericTestCase;
 import org.aksw.rdfunit.sources.CacheUtils;
 import org.aksw.rdfunit.sources.SchemaSource;
+import org.aksw.rdfunit.sources.Source;
 import org.aksw.rdfunit.sources.TestSource;
 import org.aksw.rdfunit.utils.TestUtils;
 
@@ -32,23 +33,22 @@ public class GenerateAndCacheRdfUnitTestGenerator implements RdfUnitTestGenerato
   @Override
   public Collection<? extends GenericTestCase> generate(TestSource source) {
     Collection<GenericTestCase> tests = new ArrayList<>(tagRdfUnitTestGenerator.generate(source));
-    if (!tests.isEmpty()) {
-      TestUtils.writeTestsToFile(tests,
-          new RdfFileWriter(CacheUtils.getSourceAutoTestFile(testFolder, source)));
-
-    }
+    cacheTests(tests, source);
     return tests;
   }
 
   @Override
   public Collection<? extends GenericTestCase> generate(SchemaSource source) {
     Collection<GenericTestCase> tests = new ArrayList<>(tagRdfUnitTestGenerator.generate(source));
-    if (!tests.isEmpty()) {
-      TestUtils.writeTestsToFile(tests,
-          new RdfFileWriter(CacheUtils.getSourceAutoTestFile(testFolder, source)));
-
-    }
+    cacheTests(tests, source);
     return tests;
   }
 
+  private void cacheTests(Collection<GenericTestCase> tests, Source source) {
+    if (!tests.isEmpty()) {
+      String filename = CacheUtils.getSourceAutoTestFile(testFolder, source);
+      TestUtils.writeTestsToFile(tests, new RdfFileWriter(filename));
+      log.info("{} tests written to {}", tests.size(), filename);
+    }
+  }
 }
